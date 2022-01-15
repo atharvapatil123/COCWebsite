d9e53b348a5d3715402b62130a9414dbf591b350
Atharva Patil <atharva7275@gmail.com>
2022-01-15 23:52:16 +0530

    Possible solution for Issue #140 suggested

--- new_client/src/components/Utilities/BackButton.jsx
+++ new_client/src/components/Utilities/BackButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function BackButton({ link }) {
   return (
-    <div style={{ margin: "10px" }}>
+    <div style={{ margin: "10px", marginLeft: "50px" }}>
       <span>
         <Link to={link} style={{ color: "black" }}>
           <ArrowBackIcon fontSize="large" /> Go Back
